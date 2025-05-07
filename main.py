655d9f1de6df3e1514514b08064de7736d4bb464
courelbeatriz <courelbeatriz@outlook.com>
2025-05-07 15:10:55 -0300

    Delete main.py
    Reformulando repositório

--- main.py
+++
@@ -1,8 +0,0 @@
-# Importa a classe InterfaceGrafica do arquivo interface.py
-from interface import InterfaceGrafica
-
-# Verifica se este arquivo é o principal (o ponto de entrada)
-if __name__ == "__main__":
-    # Cria uma instância da InterfaceGrafica e inicia o aplicativo
-    app = InterfaceGrafica()
-    app.mainloop()  # Inicia o loop da interface gráfica
